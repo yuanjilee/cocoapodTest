e05a8bed6924b81f8e57eb3637d150dba511a4d5
yuanjilee <824528524@qq.com>
2017-07-14 11:19:22 +0800

    add(CocoapodTest): add a framework for main project

---
+++ BFramework/BFramework/BFramework.h
@@ -0,0 +1,19 @@
+//
+//  BFramework.h
+//  BFramework
+//
+//  Created by yuanjilee on 2017/7/13.
+//  Copyright © 2017年 yuanjilee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BFramework.
+FOUNDATION_EXPORT double BFrameworkVersionNumber;
+
+//! Project version string for BFramework.
+FOUNDATION_EXPORT const unsigned char BFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BFramework/PublicHeader.h>
+
+
---
+++ BFramework/BFramework/BViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BViewController.swift
+//  BFramework
+//
+//  Created by yuanjilee on 2017/7/13.
+//  Copyright © 2017年 yuanjilee. All rights reserved.
+//
+
+import UIKit
+
+class BViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+  }
+  
+  
+}
